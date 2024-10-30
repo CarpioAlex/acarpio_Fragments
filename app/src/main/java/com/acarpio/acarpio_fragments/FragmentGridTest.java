e9652950208a9615129e0c3aecd4124e0ab066ba
Alex <acarpiomoratalla@cifpfbmoll.eu>
2024-10-30 13:37:40 +0100

    Finished first part of the exercise

---
+++ app/src/main/java/com/acarpio/acarpio_fragments/FragmentGridTest.java
@@ -0,0 +1,64 @@
+package com.acarpio.acarpio_fragments;
+
+import android.os.Bundle;
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
+
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.widget.FrameLayout;
+import android.widget.GridLayout;
+
+public class FragmentGridTest extends AppCompatActivity {
+    GridLayout layout;
+    int row = 4;
+    int column = 4;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_fragment_grid_test);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.gridLayout), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+
+        layout = findViewById(R.id.gridLayout);
+        layout.setColumnCount(column);
+        layout.setRowCount(row);
+        layout.setPadding(5, 5, 5, 5);
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < column; j++) {
+                FrameLayout frameLayout = new FrameLayout(this);
+                frameLayout.setId(View.generateViewId());
+                frameLayout.setLayoutParams(new GridLayout.LayoutParams(GridLayout.spec(i), GridLayout.spec(j)));
+                frameLayout.setPadding(5,5,5,5);
+                layout.addView(frameLayout);
+                loadFragment(new FirstFragment(), frameLayout.getId());
+
+            }
+
+
+        }
+    }
+
+    private void loadFragment(Fragment fragment, int id) {
+        FragmentManager fm = getSupportFragmentManager();
+
+        FragmentTransaction fragmentTransaction = fm.beginTransaction();
+        fragmentTransaction.replace(id, fragment);
+        fragmentTransaction.commit();
+
+
+    }
+}
---
+++ app/src/main/java/com/acarpio/acarpio_fragments/SecondFragment.java
@@ -0,0 +1,39 @@
+package com.acarpio.acarpio_fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.fragment.app.Fragment;
+
+public class SecondFragment extends Fragment {
+
+    Button button;
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_second, (ViewGroup) container, false);
+
+        button = view.findViewById(R.id.secondfragmentbutton);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(), "This is the second fragment!", Toast.LENGTH_LONG).show();
+            }
+        });
+
+
+        return view;
+
+
+
+    }
+
+
+}
